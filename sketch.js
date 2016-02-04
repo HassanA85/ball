679f45d26bc60fdb9d95ed1e2a9f9b800e4950e2
Hassan Abbas <em6097@wayne.edu>
2016-02-04 16:39:33 -0500

    Vertical bounce

--- sketch.js
+++ sketch.js
@@ -2,29 +2,33 @@ var ball = {
   x: 20,
   y: 200,
   size: 15,
+  speed: 5,
 
-  display: function () {
+  display: function() {
     // note that to access any of ball's properties, you must use dot notation, using the special keyword this
     ellipse(this.x, this.y, this.size, this.size);
   },
 
-  update: function () {
+  update: function() {
     this.checkForBounce();
     this.move();
   },
 
-  move: function () {
-    this.x += 1; // this line is the same as: this.x = this.x + 1;
+  move: function() {
+    this.y += this.speed; // this line is the same as: this.x = this.x + 1;
   },
 
-  checkForBounce: function () {
-    if (this.x > width - this.size / 2) this.bounce();
-    if (this.x < 0 + this.size / 2) this.bounce();
+  checkForBounce: function() {
+    if (this.y > height - this.size / 2) this.bounce();
+    if (this.y < 0 + this.size / 2) this.bounce1();
   },
 
-  bounce: function () {
-    // your code goes here; a hint: this will require making your own variable as well as defining this function
+  bounce: function() {
+    this.speed = this.speed - 5; // your code goes here; a hint: this will require making your own variable as well as defining this function
     // another hint: notice that ball is defined using object literal notation
+  },
+  bounce1: function() {
+    this.speed = this.speed + 5;
   }
 };
 
