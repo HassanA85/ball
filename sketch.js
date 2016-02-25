ccbf936e9c0f443e1896f8871e72fb499101f944
Hassan Abbas <em6097@wayne.edu>
2016-02-25 15:16:57 -0500

    Bounce in both directions

--- sketch.js
+++ sketch.js
@@ -2,9 +2,10 @@ var ball = {
   x: 20,
   y: 200,
   size: 15,
+  speedX: 5, //speed for horizontal boune
+  speedY: 5, //speed for vertical bounce
 
   display: function () {
-    // note that to access any of ball's properties, you must use dot notation, using the special keyword this
     ellipse(this.x, this.y, this.size, this.size);
   },
 
@@ -14,26 +15,33 @@ var ball = {
   },
 
   move: function () {
-    this.x += 1; // this line is the same as: this.x = this.x + 1;
+    this.x += this.speedX;
+    this.y += this.speedY;
   },
 
   checkForBounce: function () {
-    if (this.x > width - this.size / 2) this.bounce();
+    if (this.x > width - this.size / 2) this.reverseBounce * this.bounce ();
     if (this.x < 0 + this.size / 2) this.bounce();
-  },
+    if (this.y > height - this.size / 2) this.reverseBounce();
+    if (this.y < 0 + this.size / 2) this.bounce * this.reverseBounce();
+   },
 
   bounce: function () {
-    // your code goes here; a hint: this will require making your own variable as well as defining this function
-    // another hint: notice that ball is defined using object literal notation
-  }
+    this.speedX *= -1;
+  },
+
+  reverseBounce: function () {
+    this.speedY *= -1;
+  },
+
 };
 
-var setup = function() { // setup is called only when the sketch starts
+var setup = function() {
   createCanvas(600, 400);
 };
 
-var draw = function() { // draw is called 60 times per second; animation goes here.
-  background(100); // pay special attention to this line; what does it do?
+var draw = function() {
+  background(100);
   ball.display();
   ball.update();
 };
